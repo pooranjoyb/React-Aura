ddf22d8bdba0224330893cca1a527d79d7df1bcb
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-10-03 16:47:55 +0530

    (changes): Components Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // import React from "react"
 import GlassNavbar from "./Components/Navbars/GlassNavbar/GlassNavbar"
 import Sidebar from "./Components/Sidebar"
+import CodeBox from "./Components/CodeBox"
+import CssCopy from "./Components/CssCopy"
+import Footer from "./Components/footer/Footer"
 
 
 function App() {
@@ -8,14 +11,10 @@ function App() {
   return (
     <>
       <GlassNavbar/>
-<<<<<<< HEAD
       <CodeBox/>
       <CssCopy/>
       <Footer />
-=======
       <Sidebar />
-
->>>>>>> ae58aa049ba19c6be3e194bc6435835b08ff31d7
     </>
   )
 }
